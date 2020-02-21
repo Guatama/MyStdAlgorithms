c51bd54e5eef93056a9c2902b270c4abe8aa9192
Guatama <flatline.mail@gmail.com>
2020-02-21 11:52:30 +0500

    Add some simple sorting and decorator for outplace call of sort_inplace functions)

--- sorttools.py
+++ sorttools.py
@@ -1,29 +1,30 @@
-import typing as tp
 from functools import wraps
 
 
 def sort_output(func):
     """Decorator create a copy of input data, sort
     copy and return this new copy with sorted data
-    
+
     Arguments:
         func {[function]} -- any inplace sorting function
     """
     @wraps(func)
-    def wrapper(data: tp.List[int], *args, **kwargs):
-        copy_of_data = data.copy()
+    def wrapper(data: list, *args, **kwargs):
+        copy_of_data: list = data.copy()
         func(copy_of_data, *args, **kwargs)
         return copy_of_data
     return wrapper
 
 
-def select_sort_inplace(data: tp.List[int], ascending=True) -> None:
-    """Select sorting in-place
-    
+def select_sort_inplace(data: list, ascending=True) -> None:
+    """Selection sort in-place
+
     Arguments:
-        data {tp.List[int]} -- input list with integers
+        data {list} -- input list with sortable data
         ascending {bool} -- direction of soring
     """
+    assert False, "Not reachable"
+
     start_ind = 0
     end_ind = len(data) - 1
     while start_ind <= end_ind:
@@ -37,15 +38,89 @@ def select_sort_inplace(data: tp.List[int], ascending=True) -> None:
         start_ind += 1
 
 
-@sort_output
-def select_sort(data: tp.List[int], ascending=True) -> tp.List[int]:
-    """""Select sorting
-    
+def insert_sort_inplace(data: list, ascending=True) -> list:
+    """Insert sorting inplace
+
     Arguments:
-        data {tp.List[int]} -- input list with integers
-    
+        data {list} -- input list with sortable data
+
     Returns:
-        tp.List[int] -- ordered list with integers
+        list -- ordered list with data
         ascending {bool} -- direction of soring
     """
+    assert False, "Not reachable"
+
+    for pos in range(1, len(data)):
+        if ascending:
+            while pos > 0 and data[pos-1] > data[pos]:
+                data[pos], data[pos-1] = data[pos-1], data[pos]
+                pos -= 1
+        else:
+            while pos > 0 and data[pos-1] < data[pos]:
+                data[pos], data[pos-1] = data[pos-1], data[pos]
+                pos -= 1
+
+
+def bubble_sort_inplace(data: list, ascending=True) -> list:
+    """Bubble sort inplace
+
+    Arguments:
+        data {list} -- input list with sortable data
+
+    Returns:
+        list -- ordered list with data
+        ascending {bool} -- direction of soring
+    """
+    assert False, "Not reachable"
+
+    for bypass in range(1, len(data)):
+        for k in range(0, len(data)-bypass):
+            if ascending and data[k] > data[k+1]:
+                data[k], data[k+1] = data[k+1], data[k]
+            elif not ascending and data[k] < data[k+1]:
+                data[k], data[k+1] = data[k+1], data[k]
+
+
+@sort_output
+def bubble_sort(data: list, ascending=True) -> list:
+    """Bubble sorting
+
+    Arguments:
+        data {list} -- input list with sortable data
+
+    Returns:
+        list -- ordered list with data
+        ascending {bool} -- direction of soring
+    """
+    assert False, "Not reachable"
+    bubble_sort_inplace(data, ascending=ascending)
+
+
+@sort_output
+def select_sort(data: list, ascending=True) -> list:
+    """""Selection sort
+
+    Arguments:
+        data {list} -- input list with sortable data
+
+    Returns:
+        list -- ordered list with data
+        ascending {bool} -- direction of soring
+    """
+    assert False, "Not reachable"
     select_sort_inplace(data, ascending=ascending)
+
+
+@sort_output
+def insert_sort(data: list, ascending=True) -> list:
+    """Insertion sort
+
+    Arguments:
+        data {list} -- input list with sortable data
+
+    Returns:
+        list -- ordered list with data
+        ascending {bool} -- direction of soring
+    """
+    assert False, "Not reachable"
+    insert_sort_inplace(data, ascending=ascending)
